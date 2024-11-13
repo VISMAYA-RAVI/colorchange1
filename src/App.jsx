9d1c6d1233033331328bc5dd67bcad2efc9c3fab
VISMAYA-RAVI <ravivismaya8@gmail.com>
2024-11-13 14:19:58 +0530

    colorchange1 v1

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+import './App.css'
+
+function App() {
+  const handleClickFunction = (e) => {
+    const body = document.querySelector('body')
+    body.style.background = getRandomColor ()
+    e.target.style.backgroundColor = getRandomColor();
+  };
+
+  function getRandomColor(){
+    let letters = '0123456789ABCDEF'
+    let color = '#'
+    for(let i=0; i<6; i++){
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+
+  }
+
+  return (
+    <div>
+      <h1>Background Color Changer</h1> 
+      <button className="btn" onClick={handleClickFunction}>Click Me</button> 
+    </div>
+  )
+}
+
+export default App
